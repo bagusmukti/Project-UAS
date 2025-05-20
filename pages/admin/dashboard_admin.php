0f4cd380ae98a67902c928bdcf34e9d094988ac2
RenoAji <renoaji25sep@gmail.com>
2025-05-20 23:50:51 +0700

    Dashboard admin : urung nampilne foto karo css

--- pages/admin/dashboard_admin.php
+++ pages/admin/dashboard_admin.php
@@ -1,17 +1,17 @@
 <?php
 
-include '../config/koneksi.php';
+include '../../config/koneksi.php';
 
 // Handle search
 $search_nama = $_GET['search_nama'] ?? '';
 $search_status = $_GET['search_status'] ?? '';
 
-$query = "SELECT * FROM complaints WHERE 1=1";
+$query = "SELECT * FROM tbl_peng WHERE 1=1";
 $params = [];
 $types = '';
 
 if (!empty($search_nama)) {
-    $query .= " AND nama_pelapor LIKE ?";
+    $query .= " AND nama LIKE ?";
     $params[] = "%$search_nama";
     $types .= 's';
 }
@@ -26,6 +26,51 @@ $stmt = $conn->prepare($query);
 if (!empty($params)) {
     $stmt->bind_param($types, ...$params);
 }
+
+
 $stmt->execute();
 $result = $stmt->get_result();
 $complaints = $result->fetch_all(MYSQLI_ASSOC);
+
+$stmt->close();
+$conn->close();
+
+var_dump($complaints); // Debug, ngko hapus
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+</head>
+
+<body>
+    <h1>Dashboard Admin</h1>
+    <br>
+    <table border="1">
+        <tr>
+            <td>Nama Pelapor</td>
+            <td>Email</td>
+            <td>Isi Laporan</td>
+            <td>Foto</td>
+            <td>Aksi</td>
+        </tr>
+        <?php foreach ($complaints as $row): ?>
+            <tr>
+                <td><?= $row["nama"] ?></td>
+                <td><?= $row["email"] ?></td>
+                <td><?= $row["isi_lap"] ?></td>
+                <td><?= $row["foto"] ?></td>
+                <td>
+                    <a href="/balas.php/<?= $row["id"] ?>">Balas</a>
+                </td>
+            </tr>
+        <?php endforeach ?>
+    </table>
+</body>
+
+</html>
