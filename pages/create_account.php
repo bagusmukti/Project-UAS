2eca610271a08630296e5a30a6474a44234d81db
Bagus Mukti Purnomo <muktib301@gmail.com>
2025-05-22 20:35:59 +0700

    Update create_account.php

--- pages/create_account.php
+++ pages/create_account.php
@@ -71,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 </head>
 
 <body>
-    <h2>Buat AKun</h2>
+    <h2>Buat Akun</h2>
 
     <?php if (!empty($error)): ?>
         <div class="error"><?= $error ?></div>
