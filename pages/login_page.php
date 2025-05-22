a2ef8dc01f46d93e11e307442149d004bcee2192
YeremiaEgaWahyudi <yremiawahyudi@gmail.com>
2025-05-22 10:00:44 +0700

    Login dan Create Akun Page

---
+++ pages/login_page.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <h2>User Login</h2>
+    <form action="" method="post">
+        <div>
+            <label for="">username</label>
+            <input type="text" name="username" id="">
+        </div>
+        <div>
+            <label for="">password</label>
+            <input type="password" name="password" id="">
+        </div>
+        <div>
+            <span>Belum punya akun?</span>
+            <a href="create_account.php">Klik disini</a>
+        </div>
+        <button type="submit">Login</button>
+    </form>
+</body>
+
+</html>
