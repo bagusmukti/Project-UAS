524dcbe5f174db79a2617b4480d6a96273ef983a
Bagus Mukti Purnomo <muktib301@gmail.com>
2025-05-22 13:48:59 +0700

    tes

--- pages/logout_page.php
+++ pages/logout_page.php
@@ -4,8 +4,8 @@ session_start();
 
 if(isset($_SESSION['user_id']))
 {
-    session_unset(); // Hapus semua variabel session
-    session_destroy(); // Hapus session data
+    session_unset(); 
+    session_destroy(); 
     header("Location: ../pages/login_page.php");
     exit();
 }
