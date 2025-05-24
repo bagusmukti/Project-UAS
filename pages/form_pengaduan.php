82f2609fba0171fdc49bbb64fab772af53912ff4
Aisha Zarrah <aishazarrah@gmail.com>
2025-05-24 12:09:07 +0700

    iyah

--- pages/form_pengaduan.php
+++ pages/form_pengaduan.php
@@ -10,7 +10,7 @@
 
 <body>
     <form action="proses_pengaduan.php" method="POST" enctype="multipart/form-data" class="form-user">
-        <h2 class="h2-user">Pengaduan Masyarakat</h2>
+        <h2 class="h2-user">FORMULIR PENGADUAN</h2>
         <div>
             <label form="" class="label-user">Name</label>
             <input type="text" name="name"><br>
