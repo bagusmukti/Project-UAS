25e5d43bafd247dac15511da1e2d3106c7993f5a
Aisha Zarrah <aishazarrah@gmail.com>
2025-05-24 12:13:07 +0700

    Update form_pengaduan.php

--- pages/form_pengaduan.php
+++ pages/form_pengaduan.php
@@ -11,7 +11,7 @@
 <body>
     <form action="proses_pengaduan.php" method="POST" enctype="multipart/form-data" class="form-user">
         <h2 class="h2-user">FORMULIR PENGADUAN</h2>
-        <br><hr><br><br><br>
+        <hr><br>
         <div>
             <label form="" class="label-user">Name</label>
             <input type="text" name="name"><br>
