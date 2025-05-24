7558bc003e1d7750e58f7123624ef58cb0dfe866
Aisha Zarrah <aishazarrah@gmail.com>
2025-05-24 12:00:26 +0700

    tbl

--- pages/dashboard_user.php
+++ pages/dashboard_user.php
@@ -61,17 +61,17 @@ catch (Exception $e) {
     <div class="table-container">
         <table cellpadding="10" cellspacing="0" class="tabellll">
             <tr>
-                <td>
+                <td class="header-db">
                     <h2 class="text-dashboard">Dashboard User</h2>
                 </td>
-                <td>
+                <td class="header-buat">
+                    <a href="form_pengaduan.php" class="btn-buataduan">Buat Aduan</a>
+                </td>
+                <td class="header-logout">
                     <a href="logout_page.php" class="btn-logout">Logout <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3">
                             <path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
                         </svg>
                     </a>
-                </td>
-                <td>
-                    <a href="form_pengaduan.php" class="btn-buataduan">Buat Aduan</a>
                 </td>
             </tr>
         </table>
