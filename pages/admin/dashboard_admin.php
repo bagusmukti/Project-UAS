f9068efd93d4a3eb75505419bec2c62238e426ee
Aisha Zarrah <aishazarrah@gmail.com>
2025-06-01 18:10:51 +0700

    nyb

--- pages/admin/dashboard_admin.php
+++ pages/admin/dashboard_admin.php
@@ -202,15 +202,15 @@ while ($row = $chartData2->fetch_assoc()) {
                             </td>
 
                             </td>
-                            <td class=""> <!-- Tindakan untuk edit dan hapus -->
+                            <td> <!-- Tindakan untuk edit dan hapus -->
                                 <a class="btn btn-tanggapi" href="edit.php?id=<?= $row['id'] ?>">Berikan Tanggapan
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="http://www.w3.org/2000/svg" width="24px" fill="#fff">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="http://www.w3.org/2000/svg" width="24px">
                                         <path d="M160-400v-80h280v80H160Zm0-160v-80h440v80H160Zm0-160v-80h440v80H160Zm360 560v-123l221-220q9-9 20-13t22-4q12 0 23 4.5t20 13.5l37 37q8 9 12.5 20t4.5 22q0 11-4 22.5T863-380L643-160H520Zm300-263-37-37 37 37ZM580-220h38l121-122-18-19-19-18-122 121v38Zm141-141-19-18 37 37-18-19Z" />
                                     </svg>
                                 </a>
                                 <a class="btn btn-delete" href="delete.php?id=<?= $row['id'] ?>"
                                     onclick="return confirm('Yakin menghapus laporan ini?')">Hapus
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="http://www.w3.org/2000/svg" width="24px" fill="#fff">
+                                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="http://www.w3.org/2000/svg" width="24px">
                                         <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z" />
                                     </svg>
                                 </a>
